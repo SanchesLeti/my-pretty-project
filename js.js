76cb2fd37b4485caa0220d3f306cea29d00fe09b
SanchesLeti <145586269+SanchesLeti@users.noreply.github.com>
2023-09-26 17:18:56 +0100

    Humidity and Wind
    real data added

--- js.js
+++ js.js
@@ -61,6 +61,13 @@ function displayWeather(response) {
 
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+ 
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
 }
 
 function search(event) {
